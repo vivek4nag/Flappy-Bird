25b7ea4026d20e1676b2b8565d805c167d39e87f
viek4nag <vivek4nag@gmail.com>
2024-09-08 15:56:48 +0530

    top pipe added

--- script.js
+++ script.js
@@ -20,6 +20,21 @@ let bird = {
     height : birdHeight
 }
 
+
+//pipes
+let pipeArray = [];
+let pipeWidth = 64; // with /height ratio = 354/3072 = 1/8
+let pipeHeight = 512;
+let pipeX = boardWidth
+let pipeY = 0
+
+let topPipeImg;
+let bottomPipeImg;
+
+//game Physics
+let velocityX = -2 // pipe's moving left speed
+
+
 window.onload = function(){
     board = document.getElementById("board")
     board.height = boardHeight
@@ -37,4 +52,44 @@ window.onload = function(){
     birdImg.onload = function(){ // an event handler that triggers once the image (birdImg) has fully loaded. It ensures that the image is only drawn after it has been successfully loaded.
         context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height)
     }
+
+    topPipeImg = new Image();
+    topPipeImg.src = "./images/toppipe.png";
+
+    bottomPipeImg = new Image();
+    bottomPipeImg.src = "./images/bottompipe.png"
+
+    requestAnimationFrame(update);
+    setInterval(placePipes, 1500) // generating new pipe evry 1.5s
+}
+
+// the main game loop which will update frames
+function update(){
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, board.width, board.height) // evertime we are clearing prev frames
+
+    //bird
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height)
+
+    //pipes
+    for(i = 0; i < pipeArray.length; i++){
+        let pipe = pipeArray[i];
+        pipe.x += velocityX
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+    }
+
+}
+
+function placePipes(){
+
+    let topPipe = {
+        img : topPipeImg,
+        x : pipeX,
+        y : pipeY,
+        width : pipeWidth,
+        height : pipeHeight,
+        passed : false // to check whether the bird has passed the pipe yet
+    }
+
+    pipeArray.push(topPipe);
 }
